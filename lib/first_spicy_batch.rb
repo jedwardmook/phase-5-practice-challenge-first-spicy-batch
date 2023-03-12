d8226db9a0a47f949ce2abbf0b09f65598d59578
JedwardMook <jedwardmook@yahoo.com>
2023-03-12 17:35:34 -0500

    commit tree b87f9797ddc64c3bf7523e1b7a762ebf331d1335

--- lib/first_spicy_batch.rb
+++ lib/first_spicy_batch.rb
@@ -5,4 +5,19 @@
 
 def first_spicy_batch(n)
   # your code here
+  first = 0
+  last = n
+
+  while first < last
+    middle = first + (last - first)/2
+
+    if is_spicy_batch(middle)
+      last = middle
+    else
+      first = middle + 1
+    end
+
+  end
+  first
+  
 end
